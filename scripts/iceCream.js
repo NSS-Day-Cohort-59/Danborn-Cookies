22ca547f5f37f933f20f4c449d428e450cffb81b
Daniel Ohrin <xqiam@outlook.com>
2022-08-31 09:49:11 -0500

    Added Borders

--- scripts/iceCream.js
+++ scripts/iceCream.js
@@ -10,7 +10,7 @@ export const iceCreamList = () => {
     htmlString += `<div id="iceCream-${iceCream.id}"class="food-column">
     <img class="food-img" alt="Food Image" src="${iceCream.img}">
     <div class="description-div">
-      <h3 class="food-name">${iceCream.name}</h3>
+      <h3 class="food-name iceCream-flavor">${iceCream.name}</h3>
       <p class="food-description">${iceCream.description}</p>
     </div>
   </div>`
